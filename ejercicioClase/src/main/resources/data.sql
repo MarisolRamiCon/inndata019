ed6d6809cf709d73339c8456dc917d652b0442b3
Alejandro <libre9320@gmail.com>
2026-02-05 20:34:51 -0600

    feat: se agrega swagger y h2 que es una base de datos temporal

---
+++ ejercicioClase/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO departamento (m2, precio, activo) VALUES (100, 50000.0, true);
+INSERT INTO departamento (m2, precio, activo) VALUES (150, 75000.0, true);
+INSERT INTO departamento (m2, precio, activo) VALUES (200, 100000.0, true);
+INSERT INTO departamento (m2, precio, activo) VALUES (120, 65000.0, true);
